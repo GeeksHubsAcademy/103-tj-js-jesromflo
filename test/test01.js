d5f99d7c2e9001d401aa0a3eceb31572259e37fd
jeromflo <jeromflo@gmail.com>
2021-07-28 17:49:42 +0200

    Trabajo realizado numero 4 y final
    El periodo de realización ha sido de unos 25 minutos, en 5 minutos tenia el algoritmo, imprimiendo los asteriscos sin espacios, y el restante ha sido para añadirle los espacios y pasar los test.

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,17 @@
 function tiers(index) {
-	return "";
+	var pinturaAsteriscos = "";
+	for (var i = 0; i <= index; i++) {
+		for (var j = 0; j < index - i && i != 0; j++) {//añadimos los espacios para hacer la matriz inversa 
+			pinturaAsteriscos += " ";
+		}
+		for (var j = 0; j < i; j++) {
+			pinturaAsteriscos += "*";//añadimos los asteriscos al final gracias a los espacios, siendo la misma cantidad de asteriscos que posicion de linea
+		}
+		if (pinturaAsteriscos != "") {//si no es vacio, salto de linea despues de cada iteraccion
+			pinturaAsteriscos += "\n";
+
+		}
+	}
+	return pinturaAsteriscos;
 }
 module.exports = tiers;
